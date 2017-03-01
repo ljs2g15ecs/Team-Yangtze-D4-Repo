fc370b9b05e8d21e239ea66494053a94506c77ce
ljs2g15ecs <ljs2g15@soton.ac.uk>
2017-03-01 23:02:13 +0000

    Bluetooth and I2C Comms
    Communication board takes input from bluetooth app then transmits the
    data to the master by I2C, the master also transmits data back to the
    comms board and then to the app.

---
+++ Bluetooth to I2C/I2c_rec.ino
@@ -0,0 +1,97 @@
+#include <Wire.h>
+
+byte  QuadData[4];                        //  [0] = Throttle  | [1] = Roll  | [2] = Pitch | [3] = Yaw
+int   xyz[3];                             //  Accelerometer Data
+byte  XYZ[3];                             //  Mapped accelerometer data
+
+
+void setup()
+{
+  Serial.begin(9600);                     //  Setup serial with PC for debug
+  Wire.begin();                           //  Setup I2C
+
+  pinMode(A0, INPUT);                     //  Analog Read Pins
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+
+  //  Timer1 setup
+
+  cli();                                  //  Disable interupts
+  TCCR1A = 0;                             //  Set Registers to 0
+  TCCR1B = 0;
+  OCR1A = 62499;                          //  Prescaler for 1Hz
+
+  TCCR1B |= (1 << WGM12);                 //  Turn on CTC mode and 256 prescaler
+  TCCR1B |= (1 << CS12) | (0 << CS11) | (0 << CS10);
+  TIMSK1 |= (1 << OCIE1A);                //  Enable timer compare interupt
+  sei();                                  //  Enable interupts
+
+  pinMode(13, OUTPUT);
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  //sendData();
+  digitalWrite(13, digitalRead(13) ^ 1);
+}
+
+void loop()
+{
+  recData();
+  accelData();
+
+  printData();
+  sendData();
+}
+
+void recData()
+{
+  Wire.requestFrom(1,4);                  //  Request 4 bytes from address 1
+  while(Wire.available() < 4);
+  QuadData[0] = Wire.read();              //  Throttle
+  QuadData[1] = Wire.read();              //  Roll
+  QuadData[2] = Wire.read();              //  Pitch
+  QuadData[3] = Wire.read();              //  Yaw
+}
+
+void sendData()
+{
+  Wire.beginTransmission(1);              //  Send to address 1
+
+  Wire.write(XYZ, 3);                     //  Send 3 bytes
+
+  Wire.endTransmission(1);
+}
+
+void printData()
+{
+  Serial.print("\nThrottle  = ");
+  Serial.print(QuadData[0]);
+  Serial.print("\t|\tRoll  = ");
+  Serial.print(QuadData[1]);
+  Serial.print("\t|\tPitch = ");
+  Serial.print(QuadData[2]);
+  Serial.print("\t|\tYaw = ");
+  Serial.print(QuadData[3]);
+
+  /*
+  Serial.print( "\nX: " );
+  Serial.print( XYZ[0] );
+  Serial.print( "\tY: " );
+  Serial.print( XYZ[1] );
+  Serial.print( "\tZ: " );
+  Serial.print( XYZ[2] );
+  */
+}
+
+void accelData()
+{
+  xyz[0] = analogRead(A0);                  //  Read accelerometer
+  xyz[1] = analogRead(A1);
+  xyz[2] = analogRead(A2);
+
+  XYZ[0] = map( xyz[0], 180, 420, 0, 255);  //  Map data to byte range
+  XYZ[1] = map( xyz[1], 180, 420, 0, 255);
+  XYZ[2] = map( xyz[2], 180, 420, 0, 255);
+}
+
---
+++ Bluetooth to I2C/bluetooth_analog.ino.ino
@@ -0,0 +1,155 @@
+/*  Code Adapted from Robin2 on Arduino Forums
+
+*/
+
+#include <Wire.h>
+#include <SoftwareSerial.h>
+SoftwareSerial BT(2, 3);                //  Pin 2 = RX  | Pin 3 = TX 
+
+#define numChars 32                     //  Number of bytes sent by BT
+char receivedChars[numChars];           //  Received string
+char tempChars[numChars];               //  Temporary string for manipulation
+
+boolean newData = false;
+
+struct XY
+{
+  byte x;
+  byte y;
+} A;
+
+byte QuadData[4];                       //  [0] = Throttle  | [1] = Roll  | [2] = Pitch | [3] = Yaw
+byte XYZ[3];                            //  Accelerometer data
+
+void setup()
+{
+  Serial.begin(9600);                   //  Setup serial with PC for debug
+  Serial.println("<Arduino is ready>");
+
+  Wire.begin(1);
+  Wire.onRequest(requestEvent);         //  Register data request event
+  Wire.onReceive(receiveEvent);         //  Register data receive event
+
+  BT.begin(9600);                       //  Setup BT Serial
+}
+
+void loop()
+{
+  recvWithStartEndMarkers();
+  showNewData();
+}
+
+void recvWithStartEndMarkers()
+{
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char startMarker = '<';               //  Start marker for BT data
+  char endMarker = '>';                 //  End marker for BT data
+  char rc;
+
+  // if( Serial.available() > 0 ) {
+  while ( BT.available() > 0 && newData == false )
+  {
+    rc = BT.read();
+    if ( recvInProgress == true )
+    {
+      if (rc != endMarker)
+      {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars)
+        {
+          ndx = numChars - 1;
+        }
+      }
+      else
+      {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+    else if ( rc == startMarker )
+    {
+      recvInProgress = true;
+    }
+  }
+}
+
+void showNewData()
+{
+  if (newData == true)
+  {
+    parseData();
+    printData();
+    newData = false;
+  }
+}
+
+void parseData()
+{
+  strcpy(tempChars, receivedChars);
+  switch ( receivedChars[0] )
+  {
+    case 'L':
+      parseXY();
+      QuadData[0] = A.y;                //  Throttle
+      QuadData[3] = A.x;                //  Roll
+      break;
+    case 'R':
+      parseXY();
+      QuadData[1] = A.y;                //  Pitch
+      QuadData[2] = A.x;                //  Yaw
+      break;
+  }
+}
+
+void parseXY()                          //  !X###Y###
+{
+  char* str =  &tempChars[1];           //  !X {###} Y###
+  str = strtok(str + 1, "Y");
+  A.x = atoi(str);
+  str = strtok(NULL, " ");              //  !X###Y  {###}
+  A.y = atoi(str);
+}
+
+void printData()
+{
+  Serial.print("\nThrottle  = ");
+  Serial.print(QuadData[0]);
+  Serial.print("  | Roll  = ");
+  Serial.print(QuadData[1]);
+  Serial.print("  | Pitch = ");
+  Serial.print(QuadData[2]);
+  Serial.print("  | Yaw = ");
+  Serial.print(QuadData[3]);
+}
+
+void requestEvent()
+{
+  Wire.write(QuadData, 4);                //  Send 4 byte array
+}
+
+void receiveEvent(int n)
+{
+  int i = 0;
+  while( Wire.available() > 0 )           //  Receive 3 bytes
+  {
+    XYZ[i] = Wire.read();                 //  Store in XYZ array
+    i++;
+  }
+
+  sendData();
+}
+
+void sendData()
+{
+  BT.print( "\nX: " );
+  BT.print( XYZ[0] );
+  BT.print( "\tY: " );
+  BT.print( XYZ[1] );
+  BT.print( "\tZ: " );
+  BT.print( XYZ[2] );
+}
+
